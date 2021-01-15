77f066b4fe6651f894e5cda738a4bb6ce451897d
zhammami <zeid.alhammami@gmail.com>
2021-01-15 10:50:59 -0500

    Update new-infra.tf

--- new-infra.tf
+++ new-infra.tf
@@ -27,13 +27,7 @@ resource "aws_s3_bucket" "data" {
     Environment = local.resource_prefix.value
   }
 
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
-    }
+    
 }
 
 
