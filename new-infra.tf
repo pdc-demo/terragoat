3548ed8349f96ea476aaf01be2391808a987f219
zhammami <zeid.alhammami@gmail.com>
2021-01-19 11:40:54 -0500

    Update new-infra.tf

--- new-infra.tf
+++ new-infra.tf
@@ -23,6 +23,12 @@ resource "aws_s3_bucket" "data" {
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"   # noboost: hosting public s3 website
   force_destroy = true
+  server_side_encryption_configuration {
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
   tags = {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
